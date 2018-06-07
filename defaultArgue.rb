ef6bedebf24c85cfb2d774824959c88e9d9dde19
Jonathan Fernandez <jfern20@gmail.com>
2018-06-07 05:07:01 +0000

    Done.

---
+++ defaultArgue.rb
@@ -0,0 +1,11 @@
+def greeting(name="Ruby programmer", language="Ruby")
+  puts "Hello, #{name}. We heard you are a great #{language} programmer."
+end
+
+greeting
+
+def welcome(title="Software programmer", education="online bootcamp" )
+  puts "Hi, #{title}. Was your education resources #{education}"
+end
+
+welcome
---
+++ greeting.rb
@@ -0,0 +1,13 @@
+def greeting
+  puts "Hello world"
+end  
+
+greeting
+greeting
+greeting
+greeting
+greeting
+greeting
+greeting
+greeting
+greeting
